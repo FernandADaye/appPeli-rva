f93cec7a64a79876b12ef1dfe0760c03f5c0ea79
Fernanda López <fer.daye.2007@gmail.com>
2023-10-15 18:09:36 -0600

    carpetas

--- AppPelis/appPelisApi/src/All/Principal/Principal.jsx
+++ AppPelis/appPelisApi/src/All/Principal/Principal.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Principal() {
   return (
     <>
-
+<h1>hola</h1>
     
     
     </>
--- AppPelis/appPelisApi/src/main.jsx
+++ AppPelis/appPelisApi/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Principal from './All/Principal/Principal'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Principal></Principal>
   </React.StrictMode>,
 )
